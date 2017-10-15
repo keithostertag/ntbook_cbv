88486964eb4449257e465dc53c84cec3fed26e85
Keith Ostertag <stuff@card16.com>
2017-10-15 18:37:42 -0400

    added some comments

--- ntbs/views.py
+++ ntbs/views.py
@@ -8,6 +8,9 @@ from ntbs.models import Ntbook
 class NtbookList(ListView):
     model = Ntbook
 
+
+# In the following, we need to include the fields for creating and updating
+# use reverse_lazy to return to the main template ntbook_list.html after completing each generic view
 class NtbookCreate(CreateView):
     model = Ntbook
     success_url = reverse_lazy('ntbook_list')
