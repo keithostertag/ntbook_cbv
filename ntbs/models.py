50efa05de1741d5f1775a451c4c488ac9bb87f9a
Keith Ostertag <stuff@card16.com>
2017-10-15 14:52:00 -0400

    added created_at to model then placed and filtered in ntbook_list

--- ntbs/models.py
+++ ntbs/models.py
@@ -1,10 +1,11 @@
 from django.db import models
-
+from django.utils import timezone
 from django.core.urlresolvers import reverse
 
 class Ntbook(models.Model):
     name = models.TextField(max_length=2048)
     meta = models.TextField(max_length=2048, blank=True)
+    created_at = models.DateTimeField(default=timezone.now)
 
 
     def __str__(self):
