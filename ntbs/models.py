38c91fa86d5de86194ce85ed78b7b0741cdb36f9
Keith Ostertag <stuff@card16.com>
2017-10-14 20:45:37 -0400

    changed to Textfield and ran migrations, added a few data, scroll not working

--- ntbs/models.py
+++ ntbs/models.py
@@ -3,8 +3,8 @@ from django.db import models
 from django.core.urlresolvers import reverse
 
 class Ntbook(models.Model):
-    name = models.CharField(max_length=2048)
-    meta = models.CharField(max_length=2048)
+    name = models.TextField(max_length=2048)
+    meta = models.TextField(max_length=2048)
 
 
     def __str__(self):
