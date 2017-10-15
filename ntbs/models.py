539013bfbda2da9e31b658391c3b920c236e5ad2
Keith Ostertag <stuff@card16.com>
2017-10-15 11:54:42 -0400

    edited meta filed to allow blank then edited ntbook_list to test for meta existence

--- ntbs/models.py
+++ ntbs/models.py
@@ -4,7 +4,7 @@ from django.core.urlresolvers import reverse
 
 class Ntbook(models.Model):
     name = models.TextField(max_length=2048)
-    meta = models.TextField(max_length=2048)
+    meta = models.TextField(max_length=2048, blank=True)
 
 
     def __str__(self):
