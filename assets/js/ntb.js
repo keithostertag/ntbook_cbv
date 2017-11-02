68eb2d1410f7d7c2a7784533516daa6955135117
Keith Ostertag <stuff@card16.com>
2017-11-02 12:39:12 -0400

    added js and media query for switching to column format for smaller devices

--- assets/js/ntb.js
+++ assets/js/ntb.js
@@ -1,10 +1,29 @@
-// use resizeable-js for ability to resize right and left panels by dragging the splitter bar
-$(".panel-one").resizable({
-  handleSelector: ".splitter",
-  resizeHeight: false
-  // resizeWidth: false
+/* use resizeable-js for ability to resize top panels by dragging the splitter bar
+checks media query to see if the flex-direction is set to column, if yes then
+we need resizeWidth: false for a vertical column format, else we need the top
+panels to be in the row configutation. In either case the splitter bar will
+enable changing the relative sizes of those two panels */
+
+$(document).ready(function()  {
+  checkSize();  // run test on initial page load
 });
 
+// Function to check css rule
+function checkSize(){
+  if ($(".panel-container").css("flex-direction") == "column"){
+    $(".panel-one").resizable({
+      handleSelector: ".splitter",
+      resizeWidth: false
+    })}
+  else {
+    $(".panel-one").resizable({
+      handleSelector: ".splitter",
+      resizeHeight: false
+      }); // end resizeable
+  } // end else
+} // end function checkSize
+
+$(window).resize(checkSize);  // check to determine panel col or row format
 
 // load info/modal page when ?/infoButton is clicked
 $('#infoButton').click(function(e) {
